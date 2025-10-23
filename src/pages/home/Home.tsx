aa7cc0053c0f48a09c549555dab8e0d026d596a6
Gabriel Messias <gabrielmessias@outlook.com>
2025-10-22 23:29:10 -0300

    iniciando projeto no vite/react e começando home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,86 @@
+function Home() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#312e81",
+        minHeight: "100vh",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          color: "white",
+          width: "100%",
+          maxWidth: "1280px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: "1rem",
+            paddingBottom: "1rem",
+          }}
+        >
+          <h2
+            style={{
+              fontSize: "3rem",
+              fontWeight: "bold",
+            }}
+          >
+            Seja Bem Vindo!
+          </h2>
+
+          <p
+            style={{
+              fontSize: "1.25rem",
+            }}
+          >
+            Expresse aqui seus pensamentos e opiniões
+          </p>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-around",
+              gap: "1rem",
+            }}
+          >
+            <div
+              style={{
+                borderRadius: "0.5rem",
+                color: "white",
+                border: "2px solid white",
+                padding: "0.5rem 1rem",
+              }}
+            >
+              Nova Postagem
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src="https://i.imgur.com/yGo0Izl.jpeg"
+            alt="Imagem Pagina Home"
+            style={{
+              width: "66%",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
