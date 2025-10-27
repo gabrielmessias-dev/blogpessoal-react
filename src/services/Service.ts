04b1795f3e18f0abe1391dd8c0f4f4e9252260cd
Gabriel Messias <gabrielmessias@outlook.com>
2025-10-27 19:19:21 -0300

    ajustando service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,23 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal-jjd1.onrender.com"
+  baseURL: "https://blogpessoal-jjd1.onrender.com",
 });
 
-export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
-  try {
-    const resposta = await api.post(url, dados);
-    setDados(resposta.data);
-  } catch (error) {
-    console.error("Erro ao cadastrar usuário:", error);
-  }
+export const cadastrarUsuario = async (
+  url: string,
+  dados: object,
+  setDados: Function
+) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
 };
 
-export const login = async (url: string, dados: Object, setDados: Function) => {
-  try {
-    const resposta = await api.post(url, dados);
-    setDados(resposta.data);
-  } catch (error) {
-    console.error("Erro ao fazer login:", error);
-  }
+export const login = async (url: string, dados: object, setDados: Function) => {
+  const resposta = await api.post(url, dados);
+  setDados(resposta.data);
 };
