4439cce31ab7deb4f19a1835884fde12338a0c6a
Gabriel Messias <gabrielmessias@outlook.com>
2025-10-30 15:40:23 -0300

    add card, lista, form e deletar postagens

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,6 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem";
+
 function Home() {
   return (
     <>
@@ -8,24 +11,16 @@ function Home() {
             <p className="text-xl">Expresse aqui seus pensamentos e opiniões</p>
 
             <div className="flex justify-around gap-4">
-              <div
-                className="rounded text-white 
-                border-white border-solid border-2 py-2 px-4"
-              >
-                Nova Postagem
-              </div>
+              <ModalPostagem />
             </div>
           </div>
 
           <div className="flex justify-center ">
-            <img
-              src=""
-              alt="Imagem Página Home"
-              className="w-2/3"
-            />
+            <img src="" alt="Imagem Página Home" className="w-2/3" />
           </div>
         </div>
       </div>
+      <ListaPostagens />
     </>
   );
 }
